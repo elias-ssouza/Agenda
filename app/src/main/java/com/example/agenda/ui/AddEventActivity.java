ada75c3acda3ba2f0dbd454755e73525df84cb7d
elias-ssouza <elias.ssouza@hotmail.com>
2022-10-21 11:17:36 -0300

    adicionando função de clicar em "Adicionar Evento"

--- app/src/main/java/com/example/agenda/ui/AddEventActivity.java
+++ app/src/main/java/com/example/agenda/ui/AddEventActivity.java
@@ -4,13 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.agenda.R;
+import com.example.agenda.databinding.ActivityAddEventBinding;
+
+
 
 public class AddEventActivity extends AppCompatActivity {
+
+    private ActivityAddEventBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_event);
+        binding = ActivityAddEventBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
